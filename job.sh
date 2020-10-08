1c83827d64b61ba7b6fe323c0dbc02b39a184bc3
christianjoha <ohrtchristian@gmail.com>
2020-10-08 21:29:15 +0200

    Made adding cmd line argument via job.sh possible.

--- job.sh
+++ job.sh
@@ -2,4 +2,11 @@
 . $(dirname "$0")/URL.sh
 DIR="$(cd "$(dirname "$0")" && pwd)"
 echo "job.sh has been entered"
-python3 $DIR/main.py $URL
+
+if [ $# -eq 1 ]
+  then
+    echo "Going to record for $1 minutes."
+    python3 $DIR/main.py $URL
+  else
+    echo "No arguments supplied. Need amount of minutes to record for."
+fi
