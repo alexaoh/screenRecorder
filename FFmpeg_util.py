2455f2d96b45507407ee8364455fd8e9cd669332
christianjoha <72573129+christianjoha@users.noreply.github.com>
2020-10-09 09:29:49 +0200

    Deleted some comments in FFmpeg_util. (#1)

--- FFmpeg_util.py
+++ FFmpeg_util.py
@@ -14,25 +14,14 @@ class FFmpeg_util:
 
     def record_screen_with_audio(self, minutes):
         """Record monitor video and audio. Save as .mk4 (uncompressed) in . directory."""
-        # The command directly below works fine from the command line, but cannot find alsa_output... from crontab for some reason!
         cmd = "/usr/bin/ffmpeg -f x11grab -video_size 1920x1080 -framerate 30 -i :0.0 -f pulse -i alsa_output.pci-0000_00_1f.3.analog-stereo.monitor -preset ultrafast -crf 18 -pix_fmt yuv420p "+self.filename+".mkv"
-        #cmd = "ffmpeg -f x11grab -video_size 1920x1080 -framerate 30 -i :0.0 -f pulse -i "+ "/etc/pulse/default.pa" +" -preset ultrafast -crf 18 -pix_fmt yuv420p "+self.filename+".mkv"
-        # Kommandoen nedenfor ga ingen lyd! Den gir connection refused via crontab uansett!
-        #cmd = "/usr/bin/ffmpeg -f x11grab -video_size 1920x1080 -framerate 30 -i :0.0 -f alsa -ac 2 -i pulse -acodec aac -strict experimental -preset ultrafast -crf 18 -pix_fmt yuv420p "+self.filename+".mkv"
         process = subprocess.Popen(cmd.split(" "))
 
-        # Still need to figure out how to change to headphones as output to avoid having output through the speakers when recording. 
-        # This way I can listen to the lecture / webpage through headphones while recording also! 
 
         time.sleep(minutes*60) # Sleeps for 'minutes' minutes. 
 
         # Finish the recording (press ctrl + c in terminal)
         process.send_signal(signal.SIGINT)
-
-        # Now need to figure out if the machine can be woken up from sleep via BIOS (or something else)
-        # And use crontab to run the Python-script with the correct url!
-        # Maybe make another script to change the crontab listing (to change the url) ? Or find a better way to change the url, 
-        # since this needs to be changed week by week. 
 
         print(Colors.GREEN + "Recording complete!" + Colors.ENDC)
         return self
